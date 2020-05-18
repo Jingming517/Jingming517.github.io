f06ae116504648747ad01f13ed94df7d980d9086
jingming517 <liujingming517@gmail.com>
2020-05-18 18:57:51 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,26 @@
 //JS按照编写顺序执行
-document.write("Hello World");
+
+document.write("......"+"<br>");     //换行
+document.write("Hello World"+"</br>");
+
+var person={
+	name:"JS",
+	age:20,
+	eat:function(){
+		alert("EAT")
+	}
+}
+
+//Array
+var cars=new Array();
+cars[0]="Saab";
+cars[1]="Volvo";
+cars[2]="BMW";
+
+document.write(person.age+"</br>");
+
+function f1(a, b){
+	return a+b;
+}
+
+document.write(f1(123, 321));
