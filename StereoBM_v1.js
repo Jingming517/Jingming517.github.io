6197755cc73c2065ce829c196e426148c02515aa
jingming517 <liujingming517@gmail.com>
2020-05-29 12:41:00 +0800

    1

--- StereoBM_v1.js
+++ StereoBM_v1.js
@@ -2,6 +2,11 @@ import {IMAGE_SETTINGS, StereoProcessor,Picture} from './lib/images.js';
 
 "use strict";
 function load(){
+	
+}
+
+function StereoBM()
+{   
 	var c1=document.getElementById("Canvas_leftimg");     
 	var ctx1=c1.getContext("2d");
 	var c2=document.getElementById("Canvas_rightimg");     
@@ -37,10 +42,8 @@ function load(){
 		imageright.src = 'RightImg.png'
 	}
 	loadRightImage();
-}
 
-function StereoBM()
-{   
+	//
 	var sp = new StereoProcessor(canvas);
 	sp.LoadImagesFromCanvas(c1, c2);
 	var depthmap = sp.GetDepthMap(80, 0.5, 0.05, 0.10);
