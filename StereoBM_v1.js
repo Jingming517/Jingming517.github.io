a11c886d4bf676a8299f4521b28fd14634e2267c
jingming517 <liujingming517@gmail.com>
2020-05-29 12:43:08 +0800

    1

--- StereoBM_v1.js
+++ StereoBM_v1.js
@@ -1,6 +1,67 @@
 import {IMAGE_SETTINGS, StereoProcessor,Picture} from './lib/images.js';
 
 "use strict";
+
+function LoadPics(cxtl, cxtr){
+ function loadLeftImage(){
+  var imageleft = new Image();
+  imageleft.onload = function(){
+   var _imgl = {
+    imgl: imageleft,
+    xl: 0,
+    yl: 0
+   }
+   ctxl.drawImage(_imgl.imgl, _imgl.xl, _imgl.yl)
+  }
+  imageleft.src = 'LeftImg.png'
+ }
+ function loadRightImage(){
+  var imageright = new Image();
+  imageright.onload = function(){
+   var _imgr = {
+    imgr: imageright,
+    xr: 0,
+    yr: 0
+   }
+   ctxr.drawImage(_imgr.imgr, _imgr.xr, _imgr.yr)
+  }
+  imageright.src = 'RightImg.png'
+ }
+ loadLeftImage();
+ loadRightImage();
+ let buttonBM = document.getElementById("btn_bm");
+ buttonBM.addEventListener("click",StereoBM(c, cl, cr),false);
+}
+
+function StereoBM(c, cl, cr)
+{   
+ var sp = new StereoProcessor(c);
+ sp.LoadImagesFromCanvas(cl, cr);
+ var depthmap = sp.GetDepthMap(80, 0.5, 0.05, 0.10);
+ sp.OutputContext.putImageData(depthmap.ToImgData(),0,0);
+}
+
+(function(){
+ var cl=document.getElementById("Canvas_leftimg");     
+ var ctxl=cl.getContext("2d");
+ var cr=document.getElementById("Canvas_rightimg");     
+ var ctxr=cr.getContext("2d");
+ var c = document.getElementById("canvas");
+ var ctx=c.getContext("2d");
+ let buttonLoad = document.getElementById("btn_load");
+ buttonLoad.addEventListener("click",LoadPics(cxtl, cxtr), false);
+})();
+
+
+
+
+
+
+/*
+
+import {IMAGE_SETTINGS, StereoProcessor,Picture} from './lib/images.js';
+
+"use strict";
 function load(){
 	
 }
@@ -54,4 +115,4 @@ function StereoBM()
 let button = document.getElementById("Button1");
 button.addEventListener("click",StereoBM,false);
 })();
-
+*/
