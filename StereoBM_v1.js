dc65928ea5a4c0cade94e86d623574ef93e44439
jingming517 <liujingming517@gmail.com>
2020-05-29 09:46:30 +0800

    var

--- StereoBM_v1.js
+++ StereoBM_v1.js
@@ -42,7 +42,7 @@ function StereoBM()
 
 	var sp = new StereoProcessor(canvas);
 	sp.LoadImagesFromCanvas(c1, c2);
-	Picture depthmap = sp.GetDepthMap(80, 0.5, 0.05, 0.10);
+	var depthmap = sp.GetDepthMap(80, 0.5, 0.05, 0.10);
 	ctx.putImageData(depthmap.ToImageData());
 }
 
